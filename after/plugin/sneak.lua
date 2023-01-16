422fa41abe0e8d7b09f3afdfaf17cae2d2b89311
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-01-15 21:42:41 -0800

    remove a ton of unused keymaps

--- after/plugin/sneak.lua
+++ after/plugin/sneak.lua
@@ -1,1 +1,2 @@
-vim.keymap.set('n', ';', '<Plug>Sneak_f', { desc = 'sneak' })
+vim.keymap.set('n', 'f', '<Plug>Sneak_f', { desc = 'sneak' })
+vim.keymap.set('n', 'F', '<Plug>Sneak_F', { desc = 'sneak' })
